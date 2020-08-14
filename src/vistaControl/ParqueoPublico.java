ab3b87c82db443e3025635e677ef61b158cf02e0
David-Jhun <david.kim1@correo.icesi.edu.co>
2020-08-14 12:48:37 -0500

    The expected changes 2 and 3 were added

--- src/vistaControl/ParqueoPublico.java
+++ src/vistaControl/ParqueoPublico.java
@@ -10,6 +10,7 @@ public class ParqueoPublico extends Application {
 
       @Override
       public void start(Stage stage) throws Exception{
+    	 stage.setTitle("Parqueo Público");
          Parent root =
             FXMLLoader.load(getClass().getResource("ParqueoPublico.fxml"));
 
